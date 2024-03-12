fe26d6c031d5b15382cfe3b7a993e53a91155041
Karan Rathi <120781007+rathi-karan@users.noreply.github.com>
2024-03-12 23:10:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "7c4426fd41cd4c56bec1faf677a57b08";
+const API_KEY = "849a4b50440e40de9575fede485782ce";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
